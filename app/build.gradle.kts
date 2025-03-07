036657bdeddfe52d4be3719239ff0e33a7d43985
Ryan Guilherme <ryanguilherme.1708@hotmail.com>
2025-03-07 00:03:39 -0300

    main activity desenvolvida, criação de classes (currencyViewModelFactory, CurrencyRepository, CurrencyViewModel)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,15 @@ android {
 }
 
 dependencies {
+//implementações inseridas por mim.
+
+    implementation (libs.squareup.retrofit)
+    implementation (libs.converter.gson)
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+    implementation (libs.androidx.lifecycle.livedata.ktx)
+    implementation (libs.kotlinx.coroutines.android)
+
+    //fim
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -59,6 +68,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
