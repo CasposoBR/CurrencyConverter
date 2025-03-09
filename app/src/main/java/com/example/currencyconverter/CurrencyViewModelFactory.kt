06cd797f9c3d97befce21237242fda8700636d77
Ryan Guilherme <ryanguilherme.1708@hotmail.com>
2025-03-08 21:55:17 -0300

    feito a logica do currencyViewModelFactory

--- app/src/main/java/com/example/currencyconverter/CurrencyViewModelFactory.kt
+++ app/src/main/java/com/example/currencyconverter/CurrencyViewModelFactory.kt
@@ -1,6 +1,14 @@
 package com.example.currencyconverter
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class CurrencyViewModelFactory(repository: CurrencyRepository) : ViewModelProvider.Factory {
+class CurrencyViewModelFactory(private val repository: CurrencyRepository) : ViewModelProvider.Factory {
+override fun <T: ViewModel> create(modelClass:Class<T>): T {
+    if (modelClass.isAssignableFrom((CurrencyViewModel::class.java))) {
+        return CurrencyViewModel(repository) as T
+        }
+throw IllegalArgumentException("Unknown ViewModel class")
+
+}
 
 }
